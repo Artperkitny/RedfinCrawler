9d930c9eabc10593df4b55abbe82737e1a1ef736
Artperkitny <Artperkitny@gmail.com>
2018-08-15 22:14:33 -0500

    tax method and other small things

--- redfin.py
+++ redfin.py
@@ -49,12 +49,11 @@ class Property():
         self.url = url
         self.soup = self._get_url_info()
         self.price = int(self.soup.find(class_="statsValue").find_all("span")[1].text.replace(",",""))
-        self.info = self.soup.find(class_="amenities-container").find_all(class_="entryItemContent")
         self._get_unit_info()
         self.num_units = len(self.unit_object_list)
         self.GRI = sum([i.rent for i in [n for n in self.unit_object_list]])
         self.vacancy = 0.07
-        self.total_expenses = sum(filter(lambda x: isinstance(x,int) or isinstance(x,float),[n[1] for n in self.expenses()]))
+        self.total_expenses = round(sum(filter(lambda x: isinstance(x,int) or isinstance(x,float),[n[1] for n in self.expenses()])),2)
 
     def _get_unit_info(self):
         self.unit_object_list = []
@@ -90,6 +89,8 @@ class Property():
             unit_obj = Unit()
             if "# of Units of Type" in unit_value:
                 num_units_of_type = unit_value['# of Units of Type']
+            else:
+                num_units_of_type = 1
             unit_value.pop('# of Units of Type', None)
             for key, value in unit_value.items():
                 setattr(unit_obj, key, value)
@@ -106,40 +107,44 @@ class Property():
     def pmt(self, interest=0.05, amortization=30, down_percent=0.2):
         return(round(1*self.price*(1-down_percent)*(interest/12)/(1-(1+(interest/12))**(-amortization*12)),2))
 
-    def rent_roll(self):
+    def rent_roll(self, print_table=True):
         rent_roll = []
         for n,unit in enumerate(self.unit_object_list):
             # unit.__dict__.keys() Loop dynamically through attributes
             rent_roll.append([n+1,unit.rent,unit.beds,unit.baths,unit.rooms,unit.sqft])
-        print(tabulate(rent_roll, headers=['Unit #', 'Rent/Month', 'Beds', 'Baths', 'Rooms', 'SqFt'], tablefmt='orgtbl'))
+        if print_table:
+            print(tabulate(rent_roll, headers=['Unit #', 'Rent/Month', 'Beds', 'Baths', 'Rooms', 'SqFt'], tablefmt='orgtbl'))
+        return rent_roll
 
-    def income_and_cashflows(self):
+    def income_and_cashflows(self, print_table=True):
         top_line = [['GRI', self.GRI],
                     ['Vacancy Allowance', -1*round(self.GRI*self.vacancy,2)],
                     ['EGI', round(self.GRI*(1-self.vacancy),2)],
                 ]
         expenses = list(map(lambda x:[x[0],x[1]*-1],[n for n in self.expenses()]))
-        bottom_line = [['Expenses', -1*self.total_expenses],
-                        ['NOI', round(self.GRI*(1-self.vacancy) - self.total_expenses,2)],
+        bottom_line = [['Expenses', round(-1*self.total_expenses,2)],
+                        ['NOI', self.GRI*(1-self.vacancy) - self.total_expenses],
                         ['PMT', -1*self.pmt()],
                         ['OCF', round(self.GRI*(1-self.vacancy) - self.total_expenses - self.pmt(),2)],
                 ]
-
-        print(tabulate(top_line+expenses+bottom_line, headers=['Item', 'Amount/Month'],tablefmt='orgtbl'))
+        if print_table:
+            print(tabulate(top_line+expenses+bottom_line, headers=['Item', 'Amount/Month'],tablefmt='orgtbl'))
+        return dict(map(lambda x:[x[0].replace("- ",""),x[1]],top_line+expenses+bottom_line))
 
     def expenses(self):
         expenses = [
-            # Tax = round(float(item_value.replace("$","").replace(",","").replace(" ","")),2)
+            ["Tax",round(float(self.soup(text=re.compile(r'Tax: '))[0].parent.get_text().split(":")[1].replace("$","").replace(",","").replace(" ",""))/12,2)],
             ["Maintenance Reserve",0.03*self.GRI],
             # Management Fee = n * num-units * risk-factor
-            ["Insurance",50 * self.num_units * 1], # risk factor = 1
+            ["Landlord Insurance",100 * self.num_units * 1], # risk factor = 1
+            ["Flood Insurance",50 * self.num_units * 1], # risk factor = 1
             # Homeowners Assoc = from-source
             ["Utilities",""],
             # ["- Electricity",0], # tenant | unit-size * num-units
             # ["- Gas",0], #tenant | unit-size * num-units
-            # Sewer = n * num-units | tenant
-            ["- Water",40 * self.num_units * 1], # unit scale factor = 1
-            ["- Scavenger",40 * self.num_units],
+            ["- Sewer", round(100*365/1000*3.95/12* self.num_units * 1,2)], # Percentage of Water Rate = 100%
+            ["- Water", round(100*365/1000*3.95/12* self.num_units * 1,2)], # 100gl/day avg @ $3.95/1000gl Data from City of Chicago Finance Department
+            ["- Scavenger",9.50 * self.num_units], #  $9.50 per month per dwelling unit
             # ["- Fuel Oil",0],
             # ["- Telephone",0],
             # ["- Other",0],
@@ -158,5 +163,6 @@ class Property():
 
         return expenses
 
-property = Property(url = "https://www.redfin.com/IL/Chicago/3227-S-Carpenter-St-60608/home/14075500")
+property = Property(url = "https://www.redfin.com/IL/Chicago/1522-W-18th-Pl-60608/home/14085247")
+property.rent_roll()
 property.income_and_cashflows()
